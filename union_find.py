29df12dc5cba1d6d013e7240699559cd3475e8dd
Aayush <137766962+aayush8991@users.noreply.github.com>
2025-04-15 19:01:42 +0000

    Updates

---
+++ union_find.py
@@ -0,0 +1,30 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return False
+        if self.rank[rootX] < self.rank[rootY]:
+            self.parent[rootX] = rootY
+        elif self.rank[rootX] > self.rank[rootY]:
+            self.parent[rootY] = rootX
+        else:
+            self.parent[rootY] = rootX
+            self.rank[rootX] += 1
+        return True
+
+# Usage
+uf = UnionFind(5)
+print("Union 0-1:", uf.union(0, 1))
+print("Union 1-2:", uf.union(1, 2))
+print("Find 2:", uf.find(2))
+print("Union 0-2 (should be False):", uf.union(0, 2))
